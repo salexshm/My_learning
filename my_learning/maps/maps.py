93aec08c430b7488f391ae4fd5628dd96ce06593
salexshm <salex.shm@gmail.com>
2024-12-07 11:16:55 +0500

    create maps

---
+++ my_learning/maps/maps.py
@@ -0,0 +1,54 @@
+import folium
+from flask import Flask, render_template
+
+from geopy.distance import geodesic
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Создание карты
+    map_object = folium.Map(location=[42.315521, 69.586942], zoom_start=12)  # Центр: Германия
+
+    # Добавление метки
+    folium.Marker([42.315521, 69.586942], tooltip="Шымкент").add_to(map_object)
+
+    coordinates = [
+        [42.320218, 69.591419],  # ЮКГУ
+        [42.318915, 69.596761],  # Тауке-Хана - Байтурсынова
+        [42.330684, 69.601671],  # Байтурсынова - Маделикожа
+        [42.329212, 69.608599]  # Маделикожа
+    ]
+
+    # for point in coordinates:
+    #     folium.Marker(point).add_to(map_object)
+
+    # Рисуем линию между точками
+    folium.PolyLine(coordinates, color="blue", weight=2.5).add_to(map_object)
+
+    # Добавление линии на карту
+    folium.PolyLine(
+        locations=coordinates,
+        color='blue',
+        weight=5,
+        opacity=0.8
+    ).add_to(map_object)
+
+    # total_distance = 0  # В километрах
+    # for i in range(len(coordinates) - 1):
+    #     segment_distance = geodesic(coordinates[i], coordinates[i + 1]).kilometers
+    #     total_distance += segment_distance
+    #     print(f"Расстояние между {coordinates[i]} и {coordinates[i + 1]}: {segment_distance:.2f} км")
+
+
+    # Сохранение карты во временный HTML-файл
+    map_object.save("templates/index.html")
+
+    return render_template("index.html")  # Возвращаем готовую страницу с картой
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# m = folium.Map(location=(42.315521, 69.586942), zoom_start=14, tiles="cartodb positron")
+
--- my_learning/users/models.py
+++ my_learning/users/models.py
@@ -2,7 +2,63 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tariff(models.Model):
+    class Meta:
+        verbose_name = "Тарифный план"
+        verbose_name_plural = "Тарифный план"
+
+    name = models.CharField(max_length=12, blank=False)
+    price = models.CharField(max_length=10, blank=False)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Device(models.Model):
+    class Meta:
+        verbose_name = "Устройство"
+        verbose_name_plural = "Устройство"
+
+    ip_address = models.CharField(max_length=15, blank=False)
+    name = models.CharField(max_length=12, blank=False)
+    mac = models.CharField(max_length=17, blank=False)
+
+    def __str__(self):
+        return f"{self.ip_address}"
+
+
+class District(models.Model):
+    class Meta:
+        verbose_name = "Район"
+        verbose_name_plural = "Район"
+
+    name = models.CharField(max_length=12, verbose_name='Район', blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Street(models.Model):
+    class Meta:
+        verbose_name = "Улица"
+        verbose_name_plural = "Улица"
+
+    district = models.ForeignKey(District, verbose_name='Район', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Улица', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class UserProfile(models.Model):
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профиль"
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=12, blank=True)
-    personal_account = models.CharField(max_length=6, blank=True)
+    phone_number = models.CharField(max_length=12, verbose_name='Телефон', blank=True)
+    address = models.ForeignKey(Street, verbose_name='Улица', on_delete=models.CASCADE, blank=True, null=True)
+    home_number = models.CharField(max_length=4, verbose_name='Номер дома', blank=True)
+    flat_number = models.CharField(max_length=4, verbose_name='Квартира', blank=True)
+    tariff = models.ForeignKey(Tariff, verbose_name='Тарифный план', on_delete=models.CASCADE, blank=True, null=True)
+    device = models.OneToOneField(Device, verbose_name='Устройство', on_delete=models.CASCADE, blank=True, null=True)
